ec0d870e8eba834cfb5572e01ffe0d1f3b17bbf8
Jose Guerrero <jl.tecnologia88@gmail.com>
2023-02-19 15:36:33 -0400

    snort.conf tar version

--- traceLog.sh
+++ traceLog.sh
@@ -3,4 +3,5 @@
 tail -n0 -F "$1" | while read LINE; do
   (echo "$LINE" | grep -e "$3") && curl -X POST --silent --data-urlencode \
     "payload={\"text\": \"$(echo $LINE | sed "s/\"/'/g")\"}" "$2";
+  
 done
